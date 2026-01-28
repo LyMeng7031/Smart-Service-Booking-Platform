bc5c73f3077d79e93e931e6d24fdf54877f8d2cb
Tith Sa <tith.sa@institute.pse.ngo>
2026-01-28 15:17:40 +0700

    put update service

--- src/main/java/com/example/smart_service/service/ServiceService.java
+++ src/main/java/com/example/smart_service/service/ServiceService.java
@@ -1,11 +1,15 @@
 package com.example.smart_service.service;
 
-import org.springframework.stereotype.Service;
-
 import com.example.smart_service.dto.request.ServiceRequest;
 import com.example.smart_service.dto.response.ServiceResponse;
+import org.springframework.stereotype.Service;
 
 @Service
 public interface ServiceService {
+
+    // Create a new service
     ServiceResponse createService(ServiceRequest request);
+
+    // Update an existing service
+    ServiceResponse updateService(Long serviceId, ServiceRequest request);
 }
